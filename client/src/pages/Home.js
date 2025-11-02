bccd77f65cadc7cbb698266100c8c15f0aad1932
RobertWalaitis <rwalaitis@gmail.com>
2025-11-01 23:21:52 -0500

    change hompage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-
   return (
-    <div style={{ textAlign: "center", marginTop: "3rem" }}>
-      <h1>Welcome to Melody Match</h1>
-      <p>Click below to go to the text input page.</p>
-      <button
-        onClick={() => navigate("/text")}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-        }}
-      >
-        Go to Text Page
-      </button>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>HOMEPAGE</h1>
+      <p>Welcome to the Melody Match project!</p>
     </div>
   );
 }
